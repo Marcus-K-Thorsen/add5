075a29255a15797e45b9ce87f6d5cec3b085bde1
Marcus <marc303i@stud.kea.dk>
2022-09-01 13:42:42 +0200

    Pusher ændring, så nu er det localhost:9090 calculate.html har nu en h1 og noget andet jeg ikke ved

--- src/main/java/com/example/add5/Controller/HomeController.java
+++ src/main/java/com/example/add5/Controller/HomeController.java
@@ -1,2 +1,26 @@
-package com.example.add5.Controller;public class HomeController {
+package com.example.add5.Controller;
+
+import com.example.add5.Model.DTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HomeController {
+  @GetMapping("/")
+  public String home() {
+
+    return "Home/Home/index";
+  }
+
+  @GetMapping("/calculate")
+  public String calculate(@ModelAttribute DTO data, Model model) {
+    String value_1 = data.getValue_1();
+    String value_2 = data.getValue_2();
+    double result = Double.parseDouble(value_1) + Double.parseDouble(value_2);
+
+    model.addAttribute("result", result);
+    return "Home/Home/calculate";
+  }
 }
